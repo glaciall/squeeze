8a7db888603ad398ab995f2423b16338b1600f53
matrixy <matrixy@me.com>
2019-03-12 01:15:46 +0800

    获取待压缩的文件、开始压缩，将压缩输出导向到管道流中

--- sender/src/main/java/cn/org/hentai/squeeze/sender/worker/FileCompressor.java
+++ sender/src/main/java/cn/org/hentai/squeeze/sender/worker/FileCompressor.java
@@ -1,8 +1,47 @@
 package cn.org.hentai.squeeze.sender.worker;
 
+import cn.org.hentai.squeeze.sender.compress.CompressUtil;
+import cn.org.hentai.squeeze.sender.util.PipedReader;
+
+import java.io.PipedInputStream;
+
 /**
  * Created by matrixy on 2019/3/11.
  */
 public class FileCompressor extends Thread
 {
+    String method;
+    int level;
+    int index;
+    CompressorManager manager;
+    PipedReader pipedReader;
+
+    public FileCompressor(int index, CompressorManager manager, String method, int level)
+    {
+        this.index = index;
+        this.manager = manager;
+        this.method = method;
+        this.level = level;
+    }
+
+    public void run()
+    {
+        while (!this.isInterrupted())
+        {
+            try
+            {
+                CompressorManager.DataFile file = manager.getFile();
+                pipedReader = new PipedReader(40960);
+                manager.watchStream(index, file.id, pipedReader);
+                CompressUtil.compressAndConvertTo(file.path, level, method, pipedReader);
+
+                pipedReader.waitForClose();
+                pipedReader = null;
+            }
+            catch(Exception ex)
+            {
+                ex.printStackTrace();
+            }
+        }
+    }
 }
