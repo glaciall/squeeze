e07416922607a77daf69a7e78b0bbf63518cf391
matrixy <matrixy@me.com>
2019-03-12 01:16:04 +0800

    压缩数据发送线程的简易流程框架

--- sender/src/main/java/cn/org/hentai/squeeze/sender/worker/FileTransfer.java
+++ sender/src/main/java/cn/org/hentai/squeeze/sender/worker/FileTransfer.java
@@ -1,5 +1,7 @@
 package cn.org.hentai.squeeze.sender.worker;
 
+import cn.org.hentai.squeeze.sender.util.PipedReader;
+
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
@@ -10,10 +12,14 @@ import java.net.SocketAddress;
  */
 public class FileTransfer extends Thread
 {
+    private int bandwidthBps;
     private InetSocketAddress receiverAddress;
+    private CompressorManager manager;
 
-    public FileTransfer(InetSocketAddress receiver)
+    public FileTransfer(CompressorManager manager, int bandwidthInBytesPerSecond, InetSocketAddress receiver)
     {
+        this.manager = manager;
+        this.bandwidthBps = bandwidthInBytesPerSecond;
         this.receiverAddress = receiver;
     }
 
@@ -21,7 +27,36 @@ public class FileTransfer extends Thread
     {
         try
         {
+            // 连接到接收方
             Socket socket = new Socket();
+
+            int sendBytes = 0;
+            while (true)
+            {
+                // 遍历全部管道
+                for (;;)
+                {
+                    PipedReader pipedReader = null;
+                    if (null == pipedReader) continue;
+
+                    if (pipedReader.isCloseReady())
+                    {
+                        // 发送单个文件结束消息包
+                        pipedReader.close();
+                        continue;
+                    }
+                    int bytesReady = pipedReader.available();
+                    if (bytesReady > 0)
+                    {
+                        // 发送一个包
+                        // 计算一下已经发送了多少字节了，以及从置零起，过去了多长时间，估算一下单位时间内的发送字节数，限制一下流量
+                    }
+                }
+            }
+
+            // 如果管道流已经关闭，则
+
+            // 带宽控制
         }
         catch(Exception ex)
         {
