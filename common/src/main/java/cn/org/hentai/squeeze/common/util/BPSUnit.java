80cdaea1f58e235a79af434a2254a136de799df6
matrixy <matrixy@me.com>
2019-03-14 22:24:35 +0800

    保留两位小数

--- common/src/main/java/cn/org/hentai/squeeze/common/util/BPSUnit.java
+++ common/src/main/java/cn/org/hentai/squeeze/common/util/BPSUnit.java
@@ -8,9 +8,10 @@ public final class BPSUnit
     public static String convert(int BPS)
     {
         char unit = 'k';
-        if (BPS > 1024 * 1024 * 1024) { BPS = BPS / 1024 / 1024 / 1024; unit = 'G'; }
-        if (BPS > 1024 * 1024) { BPS = BPS / 1024 / 1024; unit = 'M'; }
-        else BPS = BPS / 1024;
-        return String.valueOf(BPS) + unit;
+        float speed = 0;
+        if (BPS > 1024 * 1024 * 1024) { speed = BPS / 1024f / 1024 / 1024; unit = 'G'; }
+        else if (BPS > 1024 * 1024) { speed = BPS / 1024f / 1024; unit = 'M'; }
+        else speed = BPS / 1024f;
+        return String.format("%.2f%s", speed, unit);
     }
 }
